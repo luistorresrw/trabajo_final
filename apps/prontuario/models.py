33f67ff58aefb6def01c865846b015b9342d72d1
luistorresrw <luistorresrw@hotmail.com>
2014-06-17 20:50:34 -0300

    primer commit (contiene la estructura principal del proyecto)

---
+++ apps/prontuario/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import signals
+
+class UserProfile(models.Model):
+	user = models.OneToOneField(User)
+	pil  = models.CharField(max_length=3)
+	factor_vencimiento = models.IntegerField()
+	fecha_ultimo_cambio = models.DateField()
+	
+	class Meta:
+		db_table = 'UserProfile'
+
+	def user_profile(sender, instance, signal, *args, **kwargs):
+		profile, new = UserProfile.objects.get_or_create(user=instance)
+
+	signals.post_save.connect(user_profile, sender=User)
