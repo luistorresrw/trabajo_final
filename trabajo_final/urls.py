249925394bbc93386709fd2b7cf7c5cfafabbd7a
luistorresrw <luistorresrw@hotmail.com>
2014-06-20 19:33:22 -0300

    subo bootstrap funcionando, cree css/style.css con el css de login y agregue el favicon y el escudo en static/images

--- trabajo_final/urls.py
+++ trabajo_final/urls.py
@@ -7,7 +7,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'trabajo_final.views.home', name='home'),
+    url(r'^$', 'trabajo_final.views.home', name='home'),
     # url(r'^trabajo_final/', include('trabajo_final.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
