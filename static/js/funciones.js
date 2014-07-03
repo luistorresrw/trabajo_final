4d887e0cd70ac8230627a2ac8aab56d8e899ad75
luistorresrw <luistorresrw@hotmail.com>
2014-07-03 00:51:46 -0300

    subo el teclado virtual casi listo

--- static/js/funciones.js
+++ static/js/funciones.js
@@ -1,10 +1,8 @@
 /*============================================
  * FUNCION DE VALIDACION DEL FORMULARIO LOGIN
  *============================================*/
-
 $(document).ready(function() {
     $('.form_login').bootstrapValidator({
-        //message: 'Este valor no es válido',
         feedbackIcons: {
             valid: 'glyphicon glyphicon-ok',
             invalid: 'glyphicon glyphicon-remove',
@@ -12,36 +10,26 @@ $(document).ready(function() {
         },
         fields: {
             usuario: {
-                //message: 'El usuario no es válido',
                 validators: {
                     notEmpty: {
-                        //message: 'Este campo no debe estas vacío'
                     },
                     stringLength: {
                         min: 7,
-                        //max: 9,
-                        //message: 'The username must be more than 6 and less than 30 characters long'
                     },
                     regexp: {
                         regexp: /^[0-9_\.\-]+$/,
-                        //message: 'The username can only consist of alphabetical, number and underscore'
                     }
                 }
             },
             password: {
-                //message: 'El usuario no es válido',
                 validators: {
                     notEmpty: {
-                        //message: 'Este campo no debe estas vacío'
                     },
                     stringLength: {
                         min: 8,
-                        //max: 30,
-                        //message: 'The username must be more than 6 and less than 30 characters long'
                     },
                     regexp: {
                         regexp: /^(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z])./,
-                        //message: 'The username can only consist of alphabetical, number and underscore'
                     }
                 }
             }
@@ -76,3 +64,7 @@ $(document).ready(function() {
     });    
 });
 */
+
+/*=========================================================
+ * FUNCION QUE HACE APARECER EL TECLADO PARA INGRESAR PIL
+ *=========================================================*/
