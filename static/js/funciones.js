abba62e26d430f3dee41d7591ba3dda31798275f
jaimeceballos <jaimeceballos82@gmail.com>
2014-07-03 11:19:04 -0300

    teclado virtual js

--- static/js/funciones.js
+++ static/js/funciones.js
@@ -68,3 +68,7 @@ $(document).ready(function() {
 /*=========================================================
  * FUNCION QUE HACE APARECER EL TECLADO PARA INGRESAR PIL
  *=========================================================*/
+$(function () {
+    $('#pil').keypad();
+    $('#pil').keypad();
+});
