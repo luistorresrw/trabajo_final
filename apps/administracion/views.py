14f51a6fa5bd039bd6a397e275c527a0d52bcda1
jaimeceballos <jaimeceballos82@gmail.com>
2014-09-09 13:45:45 -0300

    abm-departamento-ciudad

--- apps/administracion/views.py
+++ apps/administracion/views.py
@@ -4,8 +4,8 @@ from django.template import Context, Template, RequestContext
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.core.urlresolvers import reverse
 from django.core.context_processors import csrf 
-from apps.prontuarios.forms import PaisesForm,ProvinciasForm
-from apps.prontuarios.models import RefPaises,RefProvincia
+from apps.prontuarios.forms import PaisesForm,ProvinciasForm,DepartamentosForm,CiudadesForm
+from apps.prontuarios.models import RefPaises,RefProvincia,RefDepartamentos,RefCiudades
 from datetime import date
 import random
 from django.contrib.auth import *
@@ -143,3 +143,139 @@ def remove_provincia(request,prov):
       tbody.append(fila)
   
   return render_to_response('administracion/abm.html',{'form':form,'lista':lista,'clase':clase,"columns":columns,'provincia':provincia,'tbody':tbody},context_instance=RequestContext(request))
+
+@login_required
+def departamento(request):
+  clase = "departamento"
+  columns = ["provincia","descripcion"]
+  departamento = RefDepartamentos()
+  form = DepartamentosForm()
+  if request.method == 'POST':
+    form = DepartamentosForm(request.POST)
+    print form
+    if form.is_valid():
+      departamento.provincia   = form.cleaned_data['provincia']
+      departamento.descripcion = form.cleaned_data['descripcion']
+      departamento.save()
+      form = DepartamentosForm()
+      departamento = RefDepartamentos()
+
+  lista = RefDepartamentos.objects.all()
+  tbody = []
+  for elemento in lista:
+      fila = '<td>'+elemento.provincia.descripcion+'</td><td>'+elemento.descripcion+'</td>'
+      tbody.append(fila)
+  
+  return render_to_response('administracion/abm.html',{'form':form,'lista':lista,'clase':clase,"columns":columns,'departamento':departamento,'tbody':tbody},context_instance=RequestContext(request))
+
+@login_required
+def edit_departamento(request,dto):
+  clase="departamento"
+  columns = ["provincia","descripcion"]
+  departamento = RefDepartamentos.objects.get(id=dto)
+  form = DepartamentosForm(instance=departamento)
+  if request.method == 'POST':
+    form = DepartamentosForm(request.POST)
+    if form.is_valid():
+      departamento.provincia   = form.cleaned_data['provincia'];
+      departamento.descripcion = form.cleaned_data['descripcion']
+      departamento.save()
+      form = DepartamentosForm()
+      departamento = RefDepartamentos()
+
+  lista = RefDepartamentos.objects.all()
+  tbody = []
+  for elemento in lista:
+      fila = '<td>'+elemento.provincia.descripcion+'</td><td>'+elemento.descripcion+'</td>'
+      tbody.append(fila)
+  
+  return render_to_response('administracion/abm.html',{'form':form,'lista':lista,'clase':clase,"columns":columns,'departamento':departamento,'tbody':tbody},context_instance=RequestContext(request))
+
+@login_required
+def remove_departamento(request,dto):
+  clase="departamento"
+  columns = ["provincia","descripcion"]
+  departamento = RefDepartamentos.objects.get(id=dto)
+  try:
+     departamento.delete()
+  except Exception, e:
+     raise e 
+  form = DepartamentosForm()
+  departamento = RefDepartamentos()
+  lista = RefDepartamentos.objects.all()
+  tbody = []
+  for elemento in lista:
+      fila = '<td>'+elemento.provincia.descripcion+'</td><td>'+elemento.descripcion+'</td>'
+      tbody.append(fila)
+  
+  return render_to_response('administracion/abm.html',{'form':form,'lista':lista,'clase':clase,"columns":columns,'departamento':departamento,'tbody':tbody},context_instance=RequestContext(request))  
+
+@login_required
+def ciudad(request):
+  clase = "ciudad"
+  columns = ["pais","provincia","descripcion"]
+  ciudad = RefCiudades()
+  form = CiudadesForm()
+  if request.method == 'POST':
+    form = CiudadesForm(request.POST)
+    print form
+    if form.is_valid():
+      ciudad.pais           = form.cleaned_data['pais']
+      ciudad.provincia      = form.cleaned_data['provincia']
+      ciudad.departamento   = form.cleaned_data['departamento']
+      ciudad.descripcion    = form.cleaned_data['descripcion']
+      ciudad.save()
+      form = CiudadesForm()
+      ciudad = RefCiudades()
+
+  lista = RefCiudades.objects.all()
+  tbody = []
+  for elemento in lista:
+      fila = '<td>'+elemento.pais.descripcion+'</td><td>'+elemento.provincia.descripcion+'</td><td>'+elemento.descripcion+'</td>'
+      tbody.append(fila)
+  
+  return render_to_response('administracion/abm.html',{'form':form,'lista':lista,'clase':clase,"columns":columns,'ciudad':ciudad,'tbody':tbody},context_instance=RequestContext(request))
+
+@login_required
+def edit_ciudad(request,cdd):
+  clase="ciudad"
+  columns = ["pais","provincia","descripcion"]
+  ciudad = RefCiudades.objects.get(id=cdd)
+  form = CiudadesForm(instance=ciudad)
+  if request.method == 'POST':
+    form = CiudadesForm(request.POST)
+    if form.is_valid():
+      ciudad.pais           = form.cleaned_data['pais']
+      ciudad.provincia      = form.cleaned_data['provincia']
+      ciudad.departamento   = form.cleaned_data['departamento']
+      ciudad.descripcion    = form.cleaned_data['descripcion']
+      ciudad.save()
+      form = CiudadesForm()
+      ciudad = RefCiudades()
+
+  lista = RefCiudades.objects.all()
+  tbody = []
+  for elemento in lista:
+      fila = '<td>'+elemento.pais.descripcion+'</td><td>'+elemento.provincia.descripcion+'</td><td>'+elemento.descripcion+'</td>'
+      tbody.append(fila)
+  
+  return render_to_response('administracion/abm.html',{'form':form,'lista':lista,'clase':clase,"columns":columns,'ciudad':ciudad,'tbody':tbody},context_instance=RequestContext(request))
+
+@login_required
+def remove_ciudad(request,cdd):
+  clase="ciudad"
+  columns = ["pais","provincia","descripcion"]
+  ciudad = RefCiudades.objects.get(id=cdd)
+  try:
+     ciudad.delete()
+  except Exception, e:
+     raise e 
+  form = CiudadesForm()
+  ciudad = RefCiudades()
+  lista = RefCiudades.objects.all()
+  tbody = []
+  for elemento in lista:
+      fila = '<td>'+elemento.pais.descripcion+'</td><td>'+elemento.provincia.descripcion+'</td><td>'+elemento.descripcion+'</td>'
+      tbody.append(fila)
+  
+  return render_to_response('administracion/abm.html',{'form':form,'lista':lista,'clase':clase,"columns":columns,'ciudad':ciudad,'tbody':tbody},context_instance=RequestContext(request))  
--- apps/prontuarios/forms.py
+++ apps/prontuarios/forms.py
@@ -15,3 +15,20 @@ class ProvinciasForm(forms.ModelForm):
    
     class Meta:
         model = RefProvincia
+
+class DepartamentosForm(forms.ModelForm):
+	provincia 	= forms.ModelChoiceField(widget=forms.Select(attrs=dict({'class':'form-control input-block-level','required':'required'})),queryset=RefProvincia.objects.filter(pais=RefPaises.objects.filter(descripcion__contains="ARGENTINA").values('id')))
+	descripcion = forms.CharField(widget=forms.TextInput(attrs=dict({'class':'form-control input-block-level', 'placeholder':'Nombre del Departamento','required':'required','autocomplete':'off'})),required=True)
+
+	class Meta:
+		model = RefDepartamentos
+
+class CiudadesForm(forms.ModelForm):
+	pais 			= forms.ModelChoiceField(widget=forms.Select(attrs=dict({'class':'form-control input-block-level','required':'required'})),queryset=RefPaises.objects.all())
+	provincia 		= forms.ModelChoiceField(widget=forms.Select(attrs=dict({'class':'form-control input-block-level'})),queryset=RefProvincia.objects.all())
+	departamento    = forms.ModelChoiceField(widget=forms.Select(attrs=dict({'class':'form-control input-block-level'})),queryset=RefDepartamentos.objects.all())
+	descripcion = forms.CharField(widget=forms.TextInput(attrs=dict({'class':'form-control input-block-level', 'placeholder':'Nombre de la ciudad','required':'required','autocomplete':'off'})),required=True)
+
+	class Meta:
+		model = RefCiudades
+		exclude = ('lat','longi',)
