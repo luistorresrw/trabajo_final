5b671fcbd90aabdc150c5b92371292ed78c409b8
jaimeceballos <jaimeceballos82@gmail.com>
2014-12-05 13:48:42 -0300

    comenzando gestion de usuarios

--- apps/accounts/forms.py
+++ apps/accounts/forms.py
@@ -14,3 +14,14 @@ class ChangePassForm(forms.Form):
 	password_actual	 = forms.CharField(widget=forms.PasswordInput(attrs=dict({'class':'form-control input-block-level', 'placeholder':'Password actual'},render_value=False)))
 	nuevo_password 	 = forms.CharField(widget=forms.PasswordInput(attrs=dict({'class':'form-control input-block-level', 'placeholder':'Nuevo password'},render_value=False)))
 	repetir_password = forms.CharField(widget=forms.PasswordInput(attrs=dict({'class':'form-control input-block-level', 'placeholder':'Repetir password'},render_value=False)))
+
+class UserForm(forms.ModelForm):
+	username = forms.CharField(label='Numero de Documento',widget=forms.TextInput(attrs=dict({'class':'form-control input-block-level number_input', 'placeholder':'Numero de documento','autocomplete':'off'},render_value=False)))
+	last_name = forms.CharField(label='Apellidos',widget=forms.TextInput(attrs=dict({'class':'form-control input-block-level', 'placeholder':'Apellidos','disabled':'disabled'},render_value=False)))
+	first_name = forms.CharField(label='Nombres',widget=forms.TextInput(attrs=dict({'class':'form-control input-block-level', 'placeholder':'Nombres','disabled':'disabled'},render_value=False)))
+	email = forms.EmailField(label='Correo Electronico',widget=forms.TextInput(attrs=dict({'class':'form-control input-block-level', 'placeholder':'Email','disabled':'disabled'},render_value=False)))
+	is_staff = forms.BooleanField(label="Pertenece al equipo?",required=False,widget=forms.CheckboxInput(attrs=dict({'checked':'checked'})),help_text="Deje seleccionada esta casilla si el usuario es Administrador.")
+
+	class Meta:
+		model = User
+		exclude = ('last_login','date_joined','password','is_active','groups','user_permissions','is_superuser')
--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -8,5 +8,9 @@ urlpatterns = patterns('',
 	url(r'^$', login_ok, name = 'login_ok'),
 	url(r'^change_pass/$', change_password, name='change_password'),
 	url(r'^cerrar_sesion/$', cerrar_sesion, name='cerrar_sesion'),
+	url(r'^user/$', usuarios, name='usuarios'),
+	url(r'^edit_user/(?P<id>[0-9A-Za-z]+)/$',  edit_usuarios, name='edit_user'),
+	url(r'^remove_user/(?P<id>[0-9A-Za-z]+)/$',  remove_usuarios, name='remove_user'),
+	url(r'^obtener_persona/(?P<dni>[0-9]+)/$', obtener_persona, name="obtener_persona"),
 ) 
 
